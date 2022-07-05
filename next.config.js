d7fd96bf74ba254400eb0f9825f4e18d1b970ea1
Uche <ucojiaku@gmail.com>
2022-07-05 00:06:06 -0400

    new nextconfig

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  //reactStrictMode: true,
+  exportTrailingSlash: true,
 }
 
-//module.exports = nextConfig
-module.exports = {
+module.exports = nextConfig
+/* module.exports = {
   distDir: 'out',
 }
+*/
