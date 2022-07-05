916ae73df0a033de7532c7c51ea0d97fb758e52e
Uche <ucojiaku@gmail.com>
2022-07-05 02:55:43 -0400

    navbar

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
+
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -10,12 +13,32 @@ export default function Home() {
         <meta name="description" content="MAZI NFT" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar>
+        <Container>
+        <Navbar.Brand href="#MAZI" className={styles.ImageSettings}>MAZI NFT</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Nav className="me-auto">
+      <Nav.Link href="#Zụta">Zụta</Nav.Link>
+      <Nav.Link href="#Ree">Ree</Nav.Link>
+      
+    </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+        Banye dị ka: <a href="#login">Odogwu</a>
+       </Navbar.Text>
+       </Navbar.Collapse>
+       </Container>
+    </Navbar>
+      
       <span className={styles.App_header}>
       <Image src="/odogwu.svg" alt="Odogwu" width={750} height={750} className={styles.App_logo} />
       </span>
     </div>
   )
 }
+
+export default Home
 
 
 
